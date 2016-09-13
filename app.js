0c1dedc9d3c19da4883ddf1c249fc0b99fdbc208
Patrick Ford <patrickford@msn.com>
2016-09-13 08:13:37 -0700

    Added last name and styles.css

--- app.js
+++ app.js
@@ -1,4 +1,25 @@
 $(document).ready(function() {
 
-  
+  var contacts = [];
+
+  var Contact = function(firstName, lastName) {
+    this.firstName = firstName;
+    this.lastName = lastName
+  }
+
+  $("#contact").submit(function(e) {
+    e.preventDefault();
+    var firstName = $("#firstName").val();
+    var lastName = $("#lastName").val();
+
+    $("#contactList").append("<a><li class='contactName text-info'>" + 
+      firstName + " " + lastName + "</li></a>");
+    resetForm();
+  });
+
+  function resetForm() {
+    $("#firstName").val("");
+    $("#lastName").val("");
+  }
+
 })

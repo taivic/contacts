5842ab0aa44ec5dd65b78d683354d2323dc70ec1
Patrick Ford <patrickford@msn.com>
2016-09-13 08:24:56 -0700

    Added findContact

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ $(document).ready(function() {
   var contacts = [];
 
   var Contact = function(firstName, lastName) {
+    this.fullName = firstName + " " + lastName;
     this.firstName = firstName;
     this.lastName = lastName
   }
@@ -14,12 +15,30 @@ $(document).ready(function() {
 
     $("#contactList").append("<a><li class='contactName text-info'>" + 
       firstName + " " + lastName + "</li></a>");
+
+    var newContact = new Contact(firstName, lastName);
+    contacts.push(newContact);
+
     resetForm();
+    console.log(contacts);
   });
+
+  $(document).on("click", ".contactName", function() {
+    var selectedContact = findContact($(this).text());
+    console.log(selectedContact);
+  })
 
   function resetForm() {
     $("#firstName").val("");
     $("#lastName").val("");
+  }
+
+  function findContact(name) {
+    for (var i=0; i<contacts.length; i++) {
+      if (contacts[i].fullName === name) {
+        return contacts[i];
+      }
+    }
   }
 
 })
